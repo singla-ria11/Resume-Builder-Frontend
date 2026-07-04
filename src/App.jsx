7e9486fbfa95d2afdb6d9e74033aefe951bb38dc
Ria Singla <singla.ria11@gmail.com>
2026-07-04 22:36:01 +0530

    feat: set up initial app routes and add global Google font

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+// import { Home } from "lucide-react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-export default App
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+
+        <Route path="view/:resumeId" element={<Preview />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
