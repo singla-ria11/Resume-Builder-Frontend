f2bddcd624d85c2e23e1485ae9798c06141fc55f
Ria Singla <singla.ria11@gmail.com>
2026-07-06 19:17:49 +0530

    feat: implement home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-// import React from 'react'
+import Banner from "../components/home/Banner";
+import CallToAction from "../components/home/CallToAction";
+import Features from "../components/home/Features";
+import Footer from "../components/home/Footer";
+import Hero from "../components/home/Hero";
+import Testimonial from "../components/home/Testimonial";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Banner />
+      <Hero />
+      <Features />
+      <Testimonial />
+      <CallToAction />
+      <Footer />
     </div>
   );
 };
